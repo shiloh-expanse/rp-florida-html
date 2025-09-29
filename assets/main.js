8a528ed8f5e8863331688f6a4e0c9f0c3958d923
Shiloh Pettyjohn <shiloh@expansegroup.io>
2025-09-28 21:08:40 -0700

    Initial Site Commit

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+
+// Cookie consent + basic tag loader placeholders
+(function(){
+  var el = document.getElementById('cookie-consent');
+  var btn = document.getElementById('cc-accept');
+  if(!el || !btn) return;
+  if(localStorage.getItem('fcc_cookie_ok')) return;
+  el.classList.remove('hidden');
+  btn.addEventListener('click', function(){
+    localStorage.setItem('fcc_cookie_ok','1');
+    el.classList.add('hidden');
+  });
+})();
